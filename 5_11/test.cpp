3f52807bf8f6f532861171e654c3bc7338ef5382
havefunli <1641869338@qq.com>
2024-05-11 21:09:13 +0800

    commit

---
+++ 5_11/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include"Date.h"
+using namespace std;
+
+
+
+int main(){
+	Date D1(2024, 5, 1);
+	Date D2(2025, 1, 1);
+
+	int day = D2 - D1;
+	cout << day << endl;
+
+	return 1;
+}
