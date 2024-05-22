f57d4d23a8c5b1e1c01794e6dcdb8409705dd426
havefunli <1641869338@qq.com>
2024-05-22 19:49:19 +0800

    commit

--- 5_21/Test.cpp
+++ 5_21/Test.cpp
@@ -41,25 +41,72 @@ void test_3() {
 }
 
 void test_4() {
-	//my_Vector::vector<int> v;
-	//cout << v.size() << endl;
+	my_Vector::vector<int> v;
+	cout << v.size() << endl;
 
-	vector<string> v;
-	string s = "ABCD";
-	v.push_back(s);
-	cout << v[0] << endl;
+	//vector<string> v;
+	//string s = "ABCD";
+	//v.push_back(s);
+	//cout << v[0] << endl;
 
-	s[0] = 'F';
-	cout << v[0] << endl;
+	//s[0] = 'F';
+	//cout << v[0] << endl;
 
-	v[0][0] = 'F';
-	cout << v[0] << endl;
+	//v[0][0] = 'F';
+	//cout << v[0] << endl;
 }
 
 void test_5() {
 	my_Vector::vector<int> v;
 	v.insert(v.begin(), 1);
 	v.Print();
+
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	v.Print();
+}
+
+void test_6() {
+	my_Vector::vector<int> v;
+	v.insert(v.begin(), 1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+	v.Print();
+
+	auto it = v.erase(v.begin());
+	cout << *it << endl;
+
+	v.pop_back();
+	v.Print();
+}
+
+void test_7() {
+	my_Vector::vector<int> v1{ 1, 2, 3, 4, 5 };
+	v1.Print();
+
+	my_Vector::vector<int> v2(10, 1);
+	v2.Print();
+}
+
+void test_8() {
+	my_Vector::vector<int> v1 = { 1, 2, 3, 4, };
+	v1.Print();
+
+	my_Vector::vector<int> v2 = v1;
+	v2.Print();
+}
+
+void test_9() {
+	my_Vector::vector<int> v1 = { 1, 2, 3 };
+	my_Vector::vector<int> v2 = v1;
+	v2.Print();
+
+	v2 = v1;
+	v2.Print();
 }
 
 int main() {
@@ -67,7 +114,11 @@ int main() {
 	// test_2();
 	// test_3();
 	// test_4();
-	test_5();
+	// test_5();
+	// test_6();
+	// test_7();
+	// test_8();
+	test_9();
 
-	return -1;
+	return 0;
 }
--- 5_21/vector.h
+++ 5_21/vector.h
@@ -38,6 +38,18 @@ namespace my_Vector {
 			return _end_of_storage - _start;
 		}
 
+		T& operator[](size_t i) {
+			assert(i < size());
+
+			return _start[i];
+		}
+
+		const T& operator[](size_t i) const {
+			assert(i < size());
+
+			return _start[i];
+		}
+
 		// 扩容函数
 		void reserve(size_t n) {
 			if (n > capacity()) {
@@ -67,6 +79,13 @@ namespace my_Vector {
 			_end_of_storage = nullptr;
 		}
 
+		vector(int n, const T& val = T()) {
+			reserve(n);
+			for (size_t i = 0; i < n; i++) {
+				push_back(val);
+			}
+		}
+
 		template<class InputIterator>
 		vector(InputIterator start, InputIterator end) {
 			while (start != end) {
@@ -75,6 +94,42 @@ namespace my_Vector {
 			}
 		}
 
+		vector(initializer_list<T> il) {
+			reserve(il.size());
+			for (auto e : il) {
+				push_back(e);
+			}
+		}
+
+		// 拷贝构造
+		vector(const vector<T>& v) {
+			reserve(v.capacity());
+			for (auto e : v) {
+				push_back(e);
+			}
+		}
+
+		// swap函数
+		void swap(vector<T>& v) {
+			std::swap(_start, v._start);
+			std::swap(_end, v._end);
+			std::swap(_end_of_storage, v._end_of_storage);
+		}
+
+		// 赋值运算符重载
+		vector<T>& operator=(vector<T> v) {
+			swap(v);
+
+			return *this;
+		}
+
+		// 析构元素
+		~vector() {
+			delete[] _start;
+			_start = _end = _end_of_storage = nullptr;
+		}
+
+		// 插入元素
 		iterator insert(iterator pos, const T& val) {
 			assert((pos <= _end) && (pos >= _start));
 
@@ -99,13 +154,33 @@ namespace my_Vector {
 			return pos;
 		}
 
+		// 尾插
 		void push_back(const T& val) {
 			insert(end(), val);
 		}
 
+		// 删除元素
+		iterator erase(iterator pos) {
+			assert((pos >= _start) && (pos < _end));
+
+			iterator it = pos + 1;
+			while (it != _end) {
+				*(it - 1) = *it;
+				it += 1;
+			}
+			_end -= 1;
+
+			return pos;
+		}
+
+		// 尾删
+		iterator pop_back() {
+			return erase(_end - 1);
+		}
+
 	private:
-		iterator _start;
-		iterator _end;
-		iterator _end_of_storage;
+		iterator _start = nullptr;
+		iterator _end = nullptr;
+		iterator _end_of_storage = nullptr;
 	};
 }
