d38f028af2b845b4c22aecf1a7b68d72d91f9c6a
havefunli <1641869338@qq.com>
2024-06-04 23:15:00 +0800

    commit

---
+++ 6_3/L_M_S.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <iostream>
+#include <list>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+using namespace std;
+
+namespace mySpace{
+	extern string path;
+
+	// 图书
+	class Book {
+	public:
+		Book(string name, string cate, string intro, double price, size_t date);
+
+		friend ostream& operator<<(ostream&, const Book& book);
+
+		friend void del_book();
+		friend void save_File();
+		friend void Book_query();
+
+		friend bool cmp_letter(const Book& left, const Book& right);
+		friend bool cmp_price(const Book& left, const Book& right);
+		friend bool cmp_date(const Book& left, const Book& right);
+		friend void Book_sort();
+
+	private:
+		string _name; // 书名
+		string _cate; // 类别
+		string _intro; // 简介
+		double _price; // 价格
+		size_t _date; // 日期
+	};
+	ostream& operator<<(ostream&, const Book& book);
+
+	extern list<Book> lt;
+
+	void Main_menu();
+	void book_menu();
+	void sort_menu();
+
+	void add_book();
+	void del_book();
+	void Library_management();
+
+	void read_File();
+	void save_File();
+
+	void Book_query();
+	void Book_sort();
+	void view_all_book();
+
+	bool cmp_letter(const Book& left, const Book& right);
+	bool cmp_price(const Book& left, const Book& right);
+	bool cmp_date(const Book& left, const Book& right);
+
+	void sort_by_letter();
+	void sort_by_price();
+	void sort_by_date();
+}
+
---
+++ 6_3/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include "L_M_S.h"
+using namespace std;
+using namespace mySpace;
+
+int main() {
+	read_File();
+
+	int chioce = 0;
+	do {
+		Main_menu();
+		cin >> chioce;
+		system("cls");
+
+		switch (chioce) {
+		case 0:
+			Library_management();
+			break;
+		case 1:
+			Book_query();
+			break;
+		case 2:
+			Book_sort();
+		}
+	} while (chioce != 4);
+	save_File();
+
+	return 1;
+}
